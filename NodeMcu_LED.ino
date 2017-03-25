0f6fb4d0f2f937b8a61dee4d34d23755e380b61a
Ashutosh <ashutoshfcs@kiit.ac.in>
2017-03-26 01:17:39 +0530

    NODEMCU I/O

---
+++ NodeMcu_LED.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+
+int ledPin = 13;
+
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+  WiFi.disconnect();                             //To disconnect from the already connected wifi network (if any)
+  delay(1000);
+  
+  WiFi.begin("Akash Narayan","akash@iot");       //To connect to access point Akash Narayan with a password akash@iot
+  while (!(WiFi.status() == WL_CONNECTED)){    //Check for connection establishment
+    delay(300);
+  }Serial.println("\nConnected");                //Message after the connection is established
+}
+
+
+void loop()
+{
+  digitalWrite(ledPin,HIGH);
+  delay(1000);
+  digitalWrite(ledPin,LOW);
+  delay(1000);
+
+}
+
